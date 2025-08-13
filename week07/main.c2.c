c2533f279bede6cb78da744ae9acbc790461ceea
서한울 <hwsuh06@gmail.com>
2025-08-13 23:22:57 +0900

    week07

---
+++ week07/main.c2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int sumTwo(int a, int b);
+int square(int n);
+int get_max(int x, int y);
+
+int main() {
+	int num1, num2;
+	
+	printf("Enter the integers : ");
+	scanf("%d %d", &num1, &num2);
+	
+	printf("Result - Sum : %d, Square : %d, Max : 5\n", sumTwo(num1, num2), square(num1), get_max(num1, num2));
+	
+	return 0;
+}
+
+int sumTwo(int a, int b) {
+	return a + b;
+}
+
+int square(int n) {
+	return n*n;
+}
+
+int get_max(int x, int y) {
+	if (x > y) {
+		return x;
+	} else {
+		return y;
+	}
+}

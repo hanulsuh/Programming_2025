ecfbe362534ea0ba38af110534cbe3e99d13b94e
서한울 <hwsuh06@gmail.com>
2025-08-18 00:46:22 +0900

    week09

---
+++ week09/main.c5_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+static int sum = 1; // 정적 변수 선언
+
+int main(void) {
+	int sum = 0;
+	
+	printf("sum = %d\n", sum);
+	
+	return 0;
+}
+
+/*
+extern int sum;
+
+int sum1(void) {
+	sum = sum +1;
+}
+*/

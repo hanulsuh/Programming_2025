d43237bf25ca24c2717f2bbb5c1427ccac96f086
서한울 <hwsuh06@gmail.com>
2025-08-17 20:54:18 +0900

    week06

--- week06/main.c2.c
+++ week06/main.c2.c
@@ -2,12 +2,13 @@
 
 int main(void) {
 	
-	int number;
-	int absolute_value;
+	int number; // 입력받는 숫자
+	int absolute_value; // 절댓값을 저장할 변수
 	
 	printf("Enter an integer : ");
 	scanf("%i", &number);
 	
+	// 삼항 연산자를 사용하여 절댓값을 계산
 	absolute_value = (number < 0) ? -number : number;
 	
 	printf("%d\n", absolute_value);

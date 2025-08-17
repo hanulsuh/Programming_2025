065e838d8176d7127bdcbf1badb7f6d79a5e2d7d
서한울 <hwsuh06@gmail.com>
2025-08-17 21:12:51 +0900

    week07

--- week07/main.c
+++ week07/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print_star()
+void print_star() // 별을 10번 출력하는 함수 정의
 {
  int i;
  for(i = 0; i < 10; i++)
@@ -15,3 +15,19 @@ int main(void)
 	
  return 0;
 }
+
+
+/*
+int main(void) {
+	int i;
+	
+	for(i = 0; i < 10; i++)
+	 printf("*");
+	for(i = 0; i < 10; i++)
+	 printf("*");
+	for(i = 0; i < 10; i++)
+	 printf("*");
+	 
+	return 0;
+}
+*/

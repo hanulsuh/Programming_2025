3404e2dba2c8ad0e18beb3e7d213b0973094ff3c
서한울 <hwsuh06@gmail.com>
2025-08-19 02:26:31 +0900

    week05

--- week05/main.c
+++ week05/main.c
@@ -1,20 +1,24 @@
+/* 산술 연산자를 활용하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	
+	// 정수형 변수
 	int x, y, z, m;
 	int a, b, c;
 	
+	// 변수에 값 초기화
 	x = 2;
 	z = 1;
 	a = 3;
 	b = 4;
 	c = 5;
 	
+	// 계산할 수식
 	y = a*x*x + b*x + c;
 	m = (x+y+z)/3;
 	
+	// 계산 결과 출력하기
 	printf("y = %d, m = %d", y, m);
 	
 	return 0;

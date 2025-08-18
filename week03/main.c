2cd2b16509064f2c636873b7a3ca2f5972a099a1
서한울 <hwsuh06@gmail.com>
2025-08-19 01:45:11 +0900

    week03

--- week03/main.c
+++ week03/main.c
@@ -1,16 +1,20 @@
+/* 정수와 실수를 입력받고 출력하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-	int input_int;
-	float input_float;
+	int input_int; // 입력받은 정수를 저장할 변수
+	float input_float; // 입력받은 실수를 저장할 변수
 	
+	// 정수 입력 받기
 	printf("enter an integer : ");
 	scanf("%d", &input_int);
 	
+	// 실수 입력 받기
 	printf("enter a float : ");
 	scanf("%f", &input_float);
 	
+	// 입력받은 정수와 실수 출력하기
 	printf("integer : %d, float : %f\n", input_int, input_float);
 	
 	return 0;
--- week03/main.c2.c
+++ week03/main.c2.c
@@ -1,17 +1,20 @@
+/* 문자를 입력하면 숫자 형태로 출력하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
-
-int main(void) 
-{
- char c;
- int i;
+int main(void) {
+	char c; // 입력받은 문자를 저장할 변수
+    int i; // 입력받은 문자의 숫자 형태를 저장할 변수
  
- printf("input a number : ");
- scanf("%c", &c);
+    // 문자 형태 숫자를 입력받기
+    printf("input a number : ");
+    scanf("%c", &c);
  
- i = c - '0';
- printf("The input number is %i\n", i);
+    // c에 저장된 문자 값을 숫자로 변환하기
+    i = c - '0';
+    
+    // 입력받은 문자를 숫자 형태로 출력하기
+    printf("The input number is %i\n", i);
 	
- return 0;
+    return 0;
  }

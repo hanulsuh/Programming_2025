1b0b74f598c3a0b32d4628587a7641b434c8daf2
서한울 <hwsuh06@gmail.com>
2025-08-18 22:04:12 +0900

    week10

---
+++ week10/main.c3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+/*
+int i;
+int a[SIZE] = {1, 2, 3, 4, 5};
+int b[SIZE] = {1, 2, 3, 7, 10};
+
+if (a == b) {
+	printf("배열은 같은 값을 가집니다.\n");
+} else {
+	printf("배열은 같은 값을 가집니다.\n");
+}
+*/
+
+
+/*
+int main(void) {
+	int i;
+	int grade[SIZE];
+	int score[SIZE];
+	
+	for(i = 0; i < SIZE; i++) {
+		grade[i] = rand() % 100;
+		score[i] = grade[i];
+    }
+    
+	for(i = 0; i < SIZE; i++) {
+		printf("score[%d] = %d\n", i, score[i]);		
+	}
+	
+	return 0;
+}
+*/

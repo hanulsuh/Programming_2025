8da3318223b191935dbfc8606456d60d439cf114
서한울 <hwsuh06@gmail.com>
2025-08-19 02:49:37 +0900

    week05

--- week05/main.c4.c
+++ week05/main.c4.c
@@ -1,16 +1,20 @@
+/* 연도를 입력받아 윤년인지 여부를 출력하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-	
+	//
 	int year;
 	int is_leap;
 	
+	//
 	printf("Input the year : ");
 	scanf("%d", &year);
 	
+	//
 	is_leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 	
+	//
 	printf("Is the year %d the leap year? %d\n", year, is_leap);
 	
 	return 0;

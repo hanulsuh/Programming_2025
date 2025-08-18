7ddc89f7cd8bcac8143e545b84f69b9928424b4b
서한울 <hwsuh06@gmail.com>
2025-08-19 02:44:50 +0900

    week05

--- week05/main.c3.c
+++ week05/main.c3.c
@@ -1,14 +1,16 @@
+/* 초를 입력받아 분과 초의 형태로 출력하는 프로그램 */
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+	// 입력받은 초를 저장할 변수
+	int total_seconds;
 	
-	int a;
-	
+	// 초 입력받기
 	printf("Input the second : ");
-	scanf("%d", &a);
+	scanf("%d", &total_seconds);
 	
-	printf("The time is %d : %d", a/60, a%60);
+	// 입력받은 초를 분과 초로 출력하기
+	printf("The time is %d : %d", total_seconds / 60, total_seconds % 60);
 	
 	return 0;
 }

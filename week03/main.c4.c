8feb8e255d1cc3f41446047c7b965a6024622175
서한울 <hwsuh06@gmail.com>
2025-08-19 02:22:13 +0900

    week03

--- week03/main.c4.c
+++ week03/main.c4.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* 두 개의 숫자를 입력받아 나누는 프로그램 */
 
-int main(void) 
-{
- float a, b;
+#include <stdio.h>
+
+int main(void) {
+	// 입력받은 숫자를 저장할 실수형 변수
+    float a, b;
  
- printf("enter two integers : ");
- scanf("%f, %f", &a, &b);
+    // 두 정수 입력받기
+    printf("enter two integers : ");
+    scanf("%f, %f", &a, &b);
  
- printf("%f / %f = %f\n", a, b, a / b);
+    // 두 정수를 나누고 결과값 출력하기
+    printf("%f / %f = %f\n", a, b, a / b);
  
- return 0;
+    return 0;
 }

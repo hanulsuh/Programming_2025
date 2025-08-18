ac70a5e491fcf349add32d9b8fad7a812bcbedc9
서한울 <hwsuh06@gmail.com>
2025-08-19 02:07:58 +0900

    week03

--- week03/main.c3.c
+++ week03/main.c3.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* 알파벳 문자를 입력받으면 그 다음 문자를 출력하는 프로그램 */
 
-int main(void) 
-{
- char input_char;
- char next_char;
+#include <stdio.h>
+
+int main(void) {
+	// 문자를 저장할 변수
+	char input_char, next_char;
  
- printf("enter a character : ");
- scanf("%c", &input_char);
+    // 알파벳 문자를 입력받기
+    printf("enter a character : ");
+    scanf("%c", &input_char);
  
- next_char = input_char + 1;
+    // 입력받은 변수에 1을 더해 다음 알파벳 구하기
+    next_char = input_char + 1;
  
- printf("the next character of %c (%i) is %c (%i)\n", input_char, input_char, next_char, next_char); 
+    // 입력받은 알파벳과 그 다음 알파벳 출력하기
+    printf("the next character of %c (%i) is %c (%i)\n", input_char, input_char, next_char, next_char); 
  
- return 0;
+    return 0;
 }

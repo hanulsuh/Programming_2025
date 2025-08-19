0fd8d43e485df2f8142ca4a6d32d4302a641184b
서한울 <hwsuh06@gmail.com>
2025-08-20 00:54:31 +0900

    week05

--- week05/main.c4.c
+++ week05/main.c4.c
@@ -3,18 +3,17 @@
 #include <stdio.h>
 
 int main(void) {
-	//
-	int year;
-	int is_leap;
+	int year; // 입력받은 연도를 저장할 변수
+	int is_leap; // 윤년 여부를 저장하는 변수 (1 : 윤년, 0 : 평년)
 	
-	//
+	// 연도 입력받기
 	printf("Input the year : ");
 	scanf("%d", &year);
 	
-	//
+	// 윤년인지 판별하기
 	is_leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 	
-	//
+	// 입력받은 연도가 윤년인지 출력하기
 	printf("Is the year %d the leap year? %d\n", year, is_leap);
 	
 	return 0;

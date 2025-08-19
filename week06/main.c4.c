4619e8d01f2f851d9e8e44740bbbfb658814de88
서한울 <hwsuh06@gmail.com>
2025-08-20 02:06:58 +0900

    week06

--- week06/main.c4.c
+++ week06/main.c4.c
@@ -1,19 +1,21 @@
+/* 정수를 입력받아 1부터 입력된 정수까지 더한 결과를 출력하는 프로그램 */
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+	int number; // 입력받은 정수를 저장할 변수
+	int sum = 0; // 덧셈 결과를 저장할 변수
+	int i;  // for 반복문에서 사용할 카운터 변수
 	
-	int number;
-	int sum = 0;
-	int i;
-	
+	// 정수 입력받기
 	printf("Enter a number : ");
 	scanf("%d", &number);
 	
+	// for문을 활용해서 1부터 입력받은 정수까지 더하기
 	for(i = 1; i <= number; i++) {
 		sum = sum + i;
 	}
 	
+	// 덧셈 결과를 출력하기
 	printf("The result is %d\n", sum);
 	
 	return 0;

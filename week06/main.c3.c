a071c1d0f1ce9467387457ab78dcafe9d2776b10
서한울 <hwsuh06@gmail.com>
2025-08-20 01:52:45 +0900

    week06

--- week06/main.c3.c
+++ week06/main.c3.c
@@ -1,19 +1,20 @@
+/* 문자열을 입력받아 문자열 내에서 숫자의 개수를 출력하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+	int c; // getchar()로 입력받은 한 문자를 저장할 변수
+	int num = 0; // 입력받은 문자열 내 숫자 개수를 저장할 변수
 	
-	int c;
-	int num = 0;
-	
+	// 문자열을 입력받아 문자열 내 숫자 개수 세기
 	printf("Input a string : ");
-	
 	while((c = getchar()) != '\n') {
 		if (c >= '0' && c <= '9') {
 			num++;
 		}
 	}
 	
+	// 문자열 내 숫자 개수 출력하기
 	printf("The number of digits is %d\n", num);
 	
 	return 0;

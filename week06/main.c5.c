8f3f013d3274896eed01834f1f7d455bbdf15264
서한울 <hwsuh06@gmail.com>
2025-08-20 21:03:47 +0900

    week06

--- week06/main.c5.c
+++ week06/main.c5.c
@@ -1,14 +1,16 @@
+/* 두 개의 정수와 연산자를 입력 받아 계산 값을 출력하는 프로그램 */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+	int num1, num2; // 입력받은 두 개의 정수를 저장할 변수
+	char op; // 입력받은 연산자를 저장할 변수
 	
-	int num1, num2;
-	char op;
-	
+	// 두 개의 정수와 연산자를 입력받기
 	printf("Enter the calculation : ");
 	scanf("%d %c %d", &num1, &op, &num2);
 	
+	// 연산자에 따라 계산한 결과값을 출력하기
 	switch (op) {
 		case '+' :
 			printf("= %d\n", num1 + num2);

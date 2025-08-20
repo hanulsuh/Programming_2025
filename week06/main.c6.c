81ad716d0b41d2f6cf7d01d99552dc050145148d
서한울 <hwsuh06@gmail.com>
2025-08-20 21:17:46 +0900

    week06

--- week06/main.c6.c
+++ week06/main.c6.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* 숫자를 입력받아 정답 숫자보다 큰 지 작은 지 여부를 출력하고, 정답을 맞추면 시도 횟수를 출력하는 프로그램 */
 
+#include <stdio.h>
 
 int main(void) {
+	int answer = 50; // 정답 숫자를 저장하는 변수
+	int guess; // 입력받은 숫자를 저장할 변수
+	int trials = 0; // 시도 횟수를 저장할 변수
 	
-	int answer = 50;
-	int guess;
-	int trials = 0;
-	
+	// 반복적으로 답을 입력받고 정답보다 큰지 작은지 여부를 출력하기
 	do {
 		printf("Guess a number : ");
 		scanf("%d", &guess);
@@ -22,6 +22,7 @@ int main(void) {
 		}
 	} while (guess != answer);
 	
+	// 축하 메세지와 시도 횟수를 출력하기
 	printf("Congratulation! Trials : %d\n", trials);
 	
 	return 0;

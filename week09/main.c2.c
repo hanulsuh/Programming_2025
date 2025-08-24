dc19ebdb34b2951d5b6120121c6f5ef9af020e2e
서한울 <hwsuh06@gmail.com>
2025-08-24 23:50:03 +0900

    week09

--- week09/main.c2.c
+++ week09/main.c2.c
@@ -1,12 +1,16 @@
+/* 지역 변수가 속한 블록을 벗어나면 사라지는 것을 보여주는 프로그램 */
+
 #include <stdio.h>
 
 int main(void) {
 	
-	int i;
+	int i; // for 반복문에서 사용할 카운터 변수
 	
 	for(i = 0; i < 5; i++) {	
-	    int temp = 1;
-		printf("temp = %d\n", temp);
+	    int temp = 1; // 변수 선언 및 초기화
+	    
+		printf("temp = %d\n", temp); // 반복문 결과를 출력하기
+		
 		temp++;
 	}
 	

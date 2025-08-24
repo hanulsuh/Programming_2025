45f8509097888b4bcb9a0ce9643dbfbae5d868bd
서한울 <hwsuh06@gmail.com>
2025-08-25 03:10:41 +0900

    week09

--- week09/main.c5_2.c
+++ week09/main.c5_2.c
@@ -1,10 +1,13 @@
+/* 이름이 같은 지역 변수와 전역 변수의 우선순위를 보여주는 프로그램  */
 #include <stdio.h>
 
-static int sum = 1; // 정적 변수 선언
+// 정적 변수 선언
+static int sum = 1; 
 
 int main(void) {
-	int sum = 0;
+	int sum = 0; // sum 변수 선언 및 초기화
 	
+	// 결과 출력하기	
 	printf("sum = %d\n", sum);
 	
 	return 0;
@@ -12,8 +15,9 @@ int main(void) {
 
 /*
 extern int sum;
-*/
+
 
 int sum1(void) {
 	sum = sum +1;
 }
+*/

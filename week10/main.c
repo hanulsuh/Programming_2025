73d078405f6f6ff1664b84bccfc9ea39c59fbd6e
서한울 <hwsuh06@gmail.com>
2025-08-25 03:10:50 +0900

    week10

--- week10/main.c
+++ week10/main.c
@@ -1,18 +1,19 @@
+/* 다섯 학생에 대한 시험 점수 값들을 저장한 배열을 출력하는 프로그램 */
+
 #include <stdio.h>
 
 int main(void) {
-	// 변수 선언
-	int i;
-	int grade[5];
+	int i; // for문에서 사용할 카운터 변수
+	int grade[5]; // 배열 선언
 	
-	// 베얄의 각 요소에 변수 할당
+	// 배열의 각 요소에 값 할당
 	grade[0] = 10;
 	grade[1] = 20;
 	grade[2] = 30;
 	grade[3] = 40;
 	grade[4] = 50;
 	
-	// for 반복문을 사용해서 배열의 모든 요소 출력
+	// for 반복문을 사용해서 배열의 모든 요소를 출력하기
 	for (i = 0; i < 5; i++) {
 		printf("grade[%d] = %d\n", i, grade[i]);
 	}

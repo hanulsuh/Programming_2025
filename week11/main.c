0128a0ce6f2d8c6a706136ed53cb12ef3e0e6879
서한울 <hwsuh06@gmail.com>
2025-08-25 23:57:22 +0900

    week11

---
+++ week11/main.c
@@ -0,0 +1,12 @@
+/* 문자 변수와 상수를 출력하는 프로그램 */
+
+#include <stdio.h>
+
+int main(void) {
+	char code1 = 'A'; // 문자형 변수
+	int code2 = 65; // 정수형 변수
+	
+	// 문자 변수와 상수를 출력하기
+	printf("code1 = %c, (%d)\n", code1, code1);
+	printf("code2 = %c, (%d)\n", code2, code2);
+}

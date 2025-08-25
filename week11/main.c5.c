33409b7dd76ff3ce509fd9603570ada0768c97a6
서한울 <hwsuh06@gmail.com>
2025-08-26 03:03:42 +0900

    week11

--- week11/main.c5.c
+++ week11/main.c5.c
@@ -1,17 +1,22 @@
+/* 입력 받은 단어를 파일로 출력하는 프로그램 */
+
 #include <stdio.h>
 
 int main(void) {
-    FILE *fp;
-    char word[50];
+    FILE *fp; // FILE 포인터 정의
+    char word[50]; // 입력받은 단어를 저장할 배열
     
+    // 파일 열기
     fp = fopen("sample.txt", "w");
     
+    // 세 개의 단어를 입력받아 파일에 내용 쓰기
     for (int i = 0; i < 3; i++) {
     	printf("Input a word : ");
     	scanf("%s", word);
     	fprintf(fp, "%s\n", word);
 	}
 	
+	// 파일 닫기
 	fclose(fp);
 	
 	return 0;

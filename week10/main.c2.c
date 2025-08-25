789ac0af12c0653f04cec9de8a625508af65a4de
서한울 <hwsuh06@gmail.com>
2025-08-25 19:20:11 +0900

    week10

--- week10/main.c2.c
+++ week10/main.c2.c
@@ -1,43 +1,50 @@
+/* 다섯 학생의 성적을 입력 받고 성적 평균을 출력하는 프로그램 */ 
+
 #include <stdio.h>
 #define SIZE 5
 
 int main(void) {
-	// 변수 선
-	int i, average;
-	int sum = 0;
-	int grade[SIZE];
+	int i, average; // 반복문을 위한 카운터 변수와 성적 평균을 저장할 변수
+	int sum = 0; // 성적 총합을 저장할 변수
+	int grade[SIZE]; // 다섯 학생에 대한 성적 값들을 저장할 배열
 	
+	
+	// 다섯 학생의 성적을 입력 받아 총합 구하기
 	for(i = 0; i < SIZE; i++) {
-		printf("학생 성적을 입력하세요. : ");
+		printf("Enter a student's grade : ");
 		scanf("%d", &grade[i]);
 		sum += grade[i];
 	}
-	
+
 	average = sum / SIZE;
-	
-	printf("성적 평균 : %d\n", average);
-	
+
+  // 다섯 학생의 성적 평균을 출력하기
+	printf("Average grade : %d\n", average);
+
 	return 0;
 }
 
 
+/* 다섯 학생의 시험 점수를 입력받고 출력하는 프로그램 */
 /*
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 5
 
 int main(void) {
-	int i;
-	int grade[SIZE];
-
-	printf("5명의 점수를 입력하세요.\n");
-
+	int i; // for문에서 사용할 카운터 변수
+	int score[SIZE]; // 다섯 학생에 대한 시험 점수 값들을 저장할 배열
+  
+	printf("Enter 5 scores.\n");
+ 
+  // for 반복문을 사용해서 다섯 학생의 점수 입력받기
 	for(i = 0; i < SIZE; i++) {
-		scanf("%d", &grade[i]);
+		scanf("%d", &score[i]);
     }
-    
-    for(i = 0; i < SIZE; i++) {
-    	printf("grade[%d] = %d\n", i, grade[i]);
+  
+  // 입력 받은 점수를 출력하기
+  for(i = 0; i < SIZE; i++) {
+    printf("score[%d] = %d\n", i, score[i]);
 	}
 
 	return 0;
--- week10/main.c3.c
+++ week10/main.c3.c
@@ -1,35 +1,52 @@
+/* 배열을 비교해서 서로 다른 값을 가진 index를 출력하는 프로그램 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 5
 
-/*
-int i;
-int a[SIZE] = {1, 2, 3, 4, 5};
-int b[SIZE] = {1, 2, 3, 7, 10};
+int main(void) {
+    int i; // 반복문을 위한 카운터 변수
+    int a[SIZE] = {1, 2, 3, 4, 5}; // 첫 번째 배열
+    int b[SIZE] = {1, 2, 3, 7, 10}; // 두 번째 배열
 
+    // for문을 사용해서 서로 다른 값을 가진 index를 출력하기
+    for (i = 0; i < SIZE; i++) {
+        if (a[i] != b[i]) {
+            printf("Difference found at index %d\n", i);
+        }
+    }
+
+    return 0;
+}
+
+
+/*
 if (a == b) {
-	printf("배열은 같은 값을 가집니다.\n");
+	printf("Arrays have the same values.\n");
 } else {
-	printf("배열은 같은 값을 가집니다.\n");
+	printf("Arrays have different values.\n");
 }
 */
 
 
+/*  임의로 생성한 성적을 점수 배열에 복사하여 그 결과 출력하기 */
 /*
 int main(void) {
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
-	
-	for(i = 0; i < SIZE; i++) {
-		grade[i] = rand() % 100;
-		score[i] = grade[i];
+    int i; // 반복문을 위한 카운터 변수
+    int grade[SIZE]; // 성적을 저장할 배열
+    int score[SIZE]; // 점수를 저장할 배열
+    
+    // for문을 사용해서 생성한 성적 값을 점수 배열에 복사하기
+    for (i = 0; i < SIZE; i++) {
+        grade[i] = rand() % 100;
+        score[i] = grade[i];
     }
     
-	for(i = 0; i < SIZE; i++) {
-		printf("score[%d] = %d\n", i, score[i]);		
-	}
-	
-	return 0;
+    // for문을 사용해서 점수 값 출력하기
+    for (i = 0; i < SIZE; i++) {
+        printf("score[%d] = %d\n", i, score[i]);
+    }
+    
+return 0;
 }
 */

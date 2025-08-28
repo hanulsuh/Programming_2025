db17c0da049c5ed5ee72193ce4ec36b75d989a43
서한울 <hwsuh06@gmail.com>
2025-08-28 17:56:11 +0900

    week13

--- week13/main.c4.c
+++ week13/main.c4.c
@@ -1,2 +1,34 @@
 #include <stdio.h>
 
+struct point {
+	int x;
+	int y;
+};
+
+int getArea(struct point p1, struct point p2);
+
+int main(int argc, char *argv[]) {
+	struct point p1, p2;
+	
+	area = getArea(p1, p2);
+	
+	return 0;
+}
+
+int getArea(struct point p1, struct point p2) {
+	
+	return
+}
+
+/*
+int getArea(struct point *pPtr1, struct point *pPtr2);
+
+int main(int argc, char *argv[]) {
+	struct point p1, p2;
+	struct point *pPtr1, *pPtr2;
+	
+	area = getArea(pPtr1, pPtr2);
+	
+	return 0;
+}
+*/

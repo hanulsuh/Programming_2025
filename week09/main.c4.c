e1203e92f4488a7a688b7dcfab8ddc64b2432a19
서한울 <hwsuh06@gmail.com>
2025-08-29 01:10:17 +0900

    week09

--- week09/main.c4.c
+++ week09/main.c4.c
@@ -2,13 +2,13 @@
 
 #include <stdio.h>
 
-// 앞으로 사용할 함수 선언하기
+// 앞으로 사용할 함수 선언
 void sub(void);
 
 int main(void) {	
-	int i; // for 반복문에서 사용할 카운터 변수	
+	int i; // for문에서 사용할 카운터 변수	
 	
-	// for문을 사용해서 sub함수 3번 시행하기
+	// for문을 사용해서 sub함수 3번 호출
 	for(i = 0; i < 3; i++) {
 	  sub();
     }
@@ -16,14 +16,17 @@ int main(void) {
 	return 0;
 }
 
-// static allocation 과 automatic allocationi의 차이를 알아보는 함수 정의
-void sub(void) { 	
-	int auto_count = 0;
+// 자동 할당과 정적 할당의 차이를 보여주는 함수 정의
+void sub(void) { 
+	// 함수가 시행될 때마다 0으로 초기화되는 변수	
+	int auto_count = 0;          
+	// 함수가 끝나도 값을 계속 기억하는 변수
 	static int static_count = 0;
 		
 	auto_count++;
 	static_count--;
 	
+	// 각 변수의 변화를 출력
 	printf("auto_count = %d\n", auto_count);
 	printf("static_count = %d\n", static_count);
 	
